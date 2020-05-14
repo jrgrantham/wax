f5030d77a2ef45e03a21537d00f3f6bd8081a102
James Grantham <james.r.grantham@gmail.com>
2020-05-14 18:57:01 +0100

    server address

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,6 +1,6 @@
 export default function() {
   if (process.env.NODE_ENV === 'production') {
-    return 'https://jamesgrantham.herokuapp.com/'
+    return 'https://riskdb.herokuapp.com/'
   } else {
     return 'http://localhost:4500/'
   }
